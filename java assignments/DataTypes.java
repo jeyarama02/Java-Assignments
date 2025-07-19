26a477ecee9c44ef35c9840b6a9d1bcdae8cb552
jeyaram A <142777402+jeyarama02@users.noreply.github.com>
2025-07-19 17:10:15 +0530

    Add files via upload

---
+++ java assignments/DataTypes.java
@@ -0,0 +1,22 @@
+public class DataTypes
+{
+
+
+public static void main(String args[])
+{
+    int MyInt=50;
+    double MyDouble=44.44;
+    float MyFloat=23.5f;
+    long MyLong=1234567890L;
+    char MyChar='j';
+    byte MyByte=10;
+    System.out.println("INTEGER:" + " "+MyInt);
+    System.out.println("DOUBLE:" + " "+MyDouble);
+    System.out.println("FLOAT:" +" "+ MyFloat);
+    System.out.println("LONG:" + " "+MyLong);
+    System.out.println("CHAR:" + " "+MyChar);
+    System.out.println("BYTE:" + " "+MyByte);
+    
+    
+}
+}
---
+++ java assignments/Ternary.java
@@ -0,0 +1,10 @@
+public class Ternary
+{
+    public static void main (String args[])
+    {
+        int FirstNumber=10;
+        int SecondNumber=20;
+        int Max=FirstNumber>=SecondNumber ? FirstNumber:SecondNumber;
+        System.out.println("THE MAXIMUM NUMBER IS:"+ Max);
+    }
+}
