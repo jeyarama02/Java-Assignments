b2cbeabde392aff7faa061b959b56d0cba6e903d
jeyaram A <142777402+jeyarama02@users.noreply.github.com>
2025-07-29 10:44:38 +0530

    Update ConstructorStd.java

--- Day-3/ConstructorStd.java
+++ Day-3/ConstructorStd.java
@@ -20,8 +20,8 @@ public class ConstructorStd {
     }
 
     public static void main(String[] args) {
-        ConstructorStd s1 = new ConstructorStd("Sam", 1, 1234567890L, "68D - WallsStreat");
-        ConstructorStd s2 = new ConstructorStd("John", 2, 9876543210L, "26B - WallsStreat");
+        ConstructorStd s1 = new ConstructorStd("Sathish", 1, 1234567890L, "9/146 - duvarangadu");
+        ConstructorStd s2 = new ConstructorStd("ram", 2, 9876543210L, "26B - apm");
 
         s1.display();
         s2.display();
